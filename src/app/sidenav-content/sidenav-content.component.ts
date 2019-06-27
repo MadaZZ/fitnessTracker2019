fca3cdfc401efbded83aa60b5a14d08114a94455
MadaZZ <sparshranaazz@gmail.com>
2019-06-27 21:10:53 +0530

    Code optimisation for sidenav

--- src/app/sidenav-content/sidenav-content.component.ts
+++ src/app/sidenav-content/sidenav-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-content',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavContentComponent implements OnInit {
 
+  @Output() toggle = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleSidenav() {
+    this.toggle.emit();
   }
 
 }
