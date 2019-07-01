013975dfd1f3e0ff4eb108155fd3e8f254b6ccf4
MadaZZ <sparshranaazz@gmail.com>
2019-07-01 23:06:23 +0530

    Added auth actions to sidenav

--- src/app/sidenav-content/sidenav-content.component.ts
+++ src/app/sidenav-content/sidenav-content.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-sidenav-content',
   templateUrl: './sidenav-content.component.html',
   styleUrls: ['./sidenav-content.component.css']
 })
-export class SidenavContentComponent implements OnInit {
-  
+export class SidenavContentComponent implements OnInit, OnDestroy {
   @Output() toggle = new EventEmitter<void>();
-  constructor() { }
+
+  isAuth = false;
+  authSubscription: Subscription;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe( authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   toggleSidenav() {
     this.toggle.emit();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 
 }
