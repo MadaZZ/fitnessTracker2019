f42cbb3aaa74fc2e56708c0393fe3a5c3bafdd19
MadaZZ <sparshranaazz@gmail.com>
2019-07-21 22:00:12 +0530

    Fixed header navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,13 +9,17 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   @Output() toggle = new EventEmitter<void>();
-  isAuth = false;
+  private isAuth = false;
+  private authType = null;
   authSubscription: Subscription;
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.authSubscription = this.authService.authChange.subscribe( authStatus => {
       this.isAuth = authStatus;
+    });
+    this.authSubscription = this.authService.authType.subscribe( authType => {
+      this.authType = authType;
     });
   }
   toggleSidenav() {
